73c95cc2315df50de3b02c54c2256c2b5f514b72
kinodjnz <kinodjnz@gmail.com>
2022-08-18 00:11:53 +0900

    test: use dmem for buffer

--- chisel-template/src/c/fizzbuzz.c
+++ chisel-template/src/c/fizzbuzz.c
@@ -4,9 +4,9 @@ int main() {
 #define N 10
 #define cnt1(n) (((n) - (n)/3 - (n)/5))
 #define cnt2(n) (((n) - (n)/3 - (n)/5) * 2 - cnt1(N>=10 ? 10 : N))
-    char *p = fizzbuzz(N, (char *)0x2000, 256);
+    char *p = fizzbuzz(N, (char *)0x20001000, 256);
     int m = - (N / 15) * 1 + (N / 5) * 4 + (N / 3) * 4 + cnt2(N) + (N - 1) * 2;
-    return p - (char *)0x2000 - m + 1;
+    return p - (char *)0x20001000 - m + 1;
 }
 
 char *fizzbuzz(int n, char *p, int size) {
