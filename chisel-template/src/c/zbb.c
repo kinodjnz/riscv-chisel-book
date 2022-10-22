c3bfc787c73f977f8409cddb8c3f0de2042d6863
kinodjnz <kinodjnz@gmail.com>
2022-10-23 00:12:30 +0900

    test: andn/orn/xorn/zext.h/sext.b/sext.h/rev8

--- chisel-template/src/c/zbb.c
+++ chisel-template/src/c/zbb.c
@@ -78,6 +78,72 @@ int main()
 	asm volatile(".word  0x0a20d733"); // minu a4, ra, sp
 	asm volatile("li     t2, -1");
 	asm volatile("bne    a4, t2, fail");
+	asm volatile("test_14:");
+	asm volatile("li     gp, 14");
+	asm volatile("li     ra, 0x80000000");
+	asm volatile("li     sp, 0x7fffffff");
+	asm volatile(".word  0x0a20f733"); // maxu a4, ra, sp
+	asm volatile("li     t2, 0x80000000");
+	asm volatile("bne    a4, t2, fail");
+	asm volatile("test_15:");
+	asm volatile("li     gp, 15");
+	asm volatile("li     ra, 14");
+	asm volatile("li     sp, 22");
+	asm volatile(".word  0x0a20f733"); // maxu a4, ra, sp
+	asm volatile("li     t2, 22");
+	asm volatile("bne    a4, t2, fail");
+	asm volatile("test_16:");
+	asm volatile("li     gp, 16");
+	asm volatile("li     ra, -1");
+	asm volatile("li     sp, -1");
+	asm volatile(".word  0x0a20f733"); // maxu a4, ra, sp
+	asm volatile("li     t2, -1");
+	asm volatile("bne    a4, t2, fail");
+	asm volatile("test_17:");
+	asm volatile("li     gp, 17");
+	asm volatile("li     ra, 0xff4508ab");
+	asm volatile("li     sp, 0x56f00f0d");
+	asm volatile(".word  0x4020f733"); // andn a4, ra, sp
+	asm volatile("li     t2, 0xa90500a2");
+	asm volatile("bne    a4, t2, fail");
+	asm volatile("test_18:");
+	asm volatile("li     gp, 18");
+	asm volatile("li     ra, 0x0045f8ab");
+	asm volatile("li     sp, 0x56f00f3d");
+	asm volatile(".word  0x4020e733"); // orn a4, ra, sp
+	asm volatile("li     t2, 0xa94ff8eb");
+	asm volatile("bne    a4, t2, fail");
+	asm volatile("test_19:");
+	asm volatile("li     gp, 19");
+	asm volatile("li     ra, 0xff4508ab");
+	asm volatile("li     sp, 0x56f00f0d");
+	asm volatile(".word  0x4020c733"); // xnor a4, ra, sp
+	asm volatile("li     t2, 0x564af859");
+	asm volatile("bne    a4, t2, fail");
+	asm volatile("test_20:");
+	asm volatile("li     gp, 20");
+	asm volatile("li     ra, 0x0123fedc");
+	asm volatile(".word  0x0800c733"); // zext.h a4, ra
+	asm volatile("li     t2, 0xfedc");
+	asm volatile("bne    a4, t2, fail");
+	asm volatile("test_21:");
+	asm volatile("li     gp, 21");
+	asm volatile("li     ra, 0x0123fedc");
+	asm volatile(".word  0x60409713"); // sext.b a4, ra
+	asm volatile("li     t2, 0xffffffdc");
+	asm volatile("bne    a4, t2, fail");
+	asm volatile("test_22:");
+	asm volatile("li     gp, 22");
+	asm volatile("li     ra, 0x0123fedc");
+	asm volatile(".word  0x60509713"); // sext.h a4, ra
+	asm volatile("li     t2, 0xfffffedc");
+	asm volatile("bne    a4, t2, fail");
+	asm volatile("test_23:");
+	asm volatile("li     gp, 23");
+	asm volatile("li     ra, 0x0123fedc");
+	asm volatile(".word  0x6980d713"); // rev8 a4, ra
+	asm volatile("li     t2, 0xdcfe2301");
+	asm volatile("bne    a4, t2, fail");
 	asm volatile("beq    a4, t2, pass");
 	asm volatile("fail:");
 	asm volatile("fence");
